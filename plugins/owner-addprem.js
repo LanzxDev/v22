f302d8e388662fa889b41563e68b79fc26ad77b6
LanzxDev <125622522+LanzxDev@users.noreply.github.com>
2023-03-11 07:57:53 +0800

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -3,24 +3,21 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = db.data.users[who]
-    if (!who) throw `Tag Nomornya Sayang!`
+    if (!who) throw `tag atau balas pesan yang mau dijadikan premium!`
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw `Berapa Hari Ay?`
-    if (isNaN(txt)) return m.reply(`Hanya Nomor`)
+    if (!txt) throw `berapa hari mamaskuh?`
+    if (isNaN(txt)) return m.reply(`hanya nomor mamaskuh!\n\ncontoh:\n${usedPrefix + command} @${m.sender.split`@`[0]} 7`)
     var jumlahHari = 86400000 * txt
     var now = new Date() * 1
     if (now < user.premiumTime) user.premiumTime += jumlahHari
     else user.premiumTime = now + jumlahHari
-user.premium = true
-    m.reply(`Sukses
-*Nama:* ${user.name}
-*Premium:* ${txt} Hari)
+    user.premium = true
+    m.reply(`Berhasil!\n*${user.name}* sekarang sudah premium ${txt} hari.`)
 }
-handler.help = ['addprem']
+handler.help = ['addprem [@user] <hari>']
 handler.tags = ['owner']
-handler.command = /^addprem$/i
+handler.command = /^(add|tambah|\+)p(rem)?$/i
 
-handler.group = true
 handler.rowner = true
 
 export default handler
